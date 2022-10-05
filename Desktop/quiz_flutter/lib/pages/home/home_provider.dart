141b8adcb4346f9f49e365c1c4f5f70a7bd50836
Julia050889 <xlxatyanjulia@gmail.com>
2022-10-05 14:52:16 +0400

    create quiz app

---
+++ Desktop/quiz_flutter/lib/pages/home/home_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:quiz_flutter/base/route.dart';
+
+class HomePageProvider extends ChangeNotifier {
+  HomePageProvider() {
+    getData();
+  }
+
+  getData() async{
+
+  }
+}
