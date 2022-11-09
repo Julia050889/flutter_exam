37cc30da6a358d642a4e849928b5be69f6d8c998
Julia050889 <xlxatyanjulia@gmail.com>
2022-11-09 12:35:34 +0400

    reactive forms

---
+++ lib/bloc/form_state.dart
@@ -0,0 +1,33 @@
+part of 'form_bloc.dart';
+
+class FormPageState extends Equatable {
+  bool showPassword;
+
+  FormPageState({this.showPassword = false});
+
+  FormPageState copyWith({bool? showPassword}) {
+    return FormPageState(
+      showPassword: showPassword ?? this.showPassword,
+    );
+  }
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [showPassword];
+}
+
+// class FormState extends Equatable {
+//   final FormGroup? form;
+
+//   const FormState({required this.form});
+
+//   FormState copyWith({FormGroup? from}) {
+//     return FormState(
+//       form: form ?? this.form,
+//     );
+//   }
+
+//   @override
+//   // TODO: implement props
+//   List<Object?> get props => [form];
+// }
